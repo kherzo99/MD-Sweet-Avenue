059165e04fb9311e326d810fca0522371e7428c0
Karla Herzo ⚡+ <127571238+kherzo99@users.noreply.github.com>
2024-03-12 19:09:34 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const showMore = (id) => {
+  let div = document.getElementById(id);
+  if (div.style.display == "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+};
